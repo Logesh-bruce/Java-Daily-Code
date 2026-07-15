8c14891f5b643b9e1218be6fb55e508fa6a995d0
LogeshManivel <logeshmg432006@gmail.com>
2026-07-15 08:11:52 +0530

    Time: 7 ms (27.27%) | Memory: 29 MB (72.73%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int GcdOfOddEvenSums(int n) {
+        int odd=0,even=0;
+        for(int i=1;i<n*2;i++){
+            if(i%2!=0){
+odd+=i;
+            }else{
+                even+=i;
+            }
+        }return odd-even;
+    }
+}
