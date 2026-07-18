9104daf6490c599e75ff2ed1963e25bbdfd815d7
LogeshManivel <logeshmg432006@gmail.com>
2026-07-18 12:52:49 +0530

    Time: 1 ms (97.33%) | Memory: 42.8 MB (70.07%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,16 +1,22 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         int c=0,start=0;
-       HashSet<Character>set=new HashSet<>();
-       for(int i=0;i<s.length();i++){
-        while(set.contains(s.charAt(i))||set.size()==3){
-            set.remove(s.charAt(start++));
-        }
-        set.add(s.charAt(i));
-        if(set.size()==3){
-            c++;
-        }
+    //    HashSet<Character>set=new HashSet<>();
+    //    for(int i=0;i<s.length();i++){
+    //     while(set.contains(s.charAt(i))||set.size()==3){
+    //         set.remove(s.charAt(start++));
+    //     }
+    //     set.add(s.charAt(i));
+    //     if(set.size()==3){
+    //         c++;
+    //     }
         
-       }return c;
+    //    }return c;
+int d=0;
+    for(int i=0;i<=s.length()-3;i++){
+        if(s.charAt(i)!=s.charAt(i+1)&&s.charAt(i)!=s.charAt(i+2)&&s.charAt(i+2)!=s.charAt(i+1)){
+            d++;
+        }
+    }return d;
 }
 }
